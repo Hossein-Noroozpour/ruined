acae0503e2bd712f63b245467e8e92cba820d934
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2016-11-16 12:11:04 +0330

    threaded connection is done

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use std::io::Write;
-use std::net::TcpListener;
+use std::net::{
+    TcpListener,
+};
 use std::thread;
 use std::sync::{
     Arc,
     Mutex,
 };
 
-const NUM_THREADES: usize = 128;
+const NUM_THREADS: usize = 128;
 
 fn main() {
+    let mut threads = Vec::new();
     let listener = TcpListener::bind("127.0.0.1:9123").unwrap();
     println!("Listening started, ready to accept");
     let listener = Arc::new(Mutex::new(listener));
-    for i in 1..NUM_THREADES {
+    for i in 0..NUM_THREADS {
         let col_listener = listener.clone();
-        thread::spawn(move || {
+        threads.push(thread::spawn(move || {
             loop {
                 let thread_id = i;
                 let (mut stream, address) = {
@@ -24,15 +27,18 @@ fn main() {
                 stream.write(b"Hello World\r\n").unwrap();
                 let (_, _) = (thread_id, address);
             }
-        });
+        }));
     }
-    let col_listener = listener.clone();
-    loop {
-        let (mut stream, address) = {
-            let listener = col_listener.lock().unwrap();
-            listener.accept().unwrap()
-        };
-        stream.write(b"Hello World\r\n").unwrap();
-        let _ = address;
+
+//    for _ in 0..100 {
+//        let mut s = TcpStream::connect("127.0.0.1:9123").unwrap();
+//        let mut buff = [0u8; 128];
+//        use std::io::Read;
+//        s.read(&mut buff).expect("Error in reading.");
+//        println!("{} {} {} {} {} {}", buff[0], buff[1], buff[2], buff[3], buff[4], buff[5]);
+//    }
+
+    for th in threads {
+        th.join().expect("Error in joining thread.");
     }
 }
