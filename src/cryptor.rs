19e76bdd6d96a41e7d6b9488c0b570026c1b56dd
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2016-11-16 13:05:30 +0330

    cryptor is done

--- src/cryptor.rs
+++ src/cryptor.rs
@@ -14,12 +14,11 @@ use crypto::buffer::{
 };
 
 pub enum Error {
-    Size,
-    Key,
-    Iv,
+    Length,
+    Padding,
 }
 
-pub fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
+pub fn en(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
     let mut encryptor = aes::cbc_encryptor(
         aes::KeySize::KeySize256,
         key,
@@ -33,7 +32,12 @@ pub fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
         let result = match encryptor.encrypt(&mut read_buffer, &mut write_buffer, true) {
             Ok(r) => r,
             Err(e) => {
-
+                match e {
+                    symmetriccipher::SymmetricCipherError::InvalidLength =>
+                        return Err(Error::Length),
+                    symmetriccipher::SymmetricCipherError::InvalidPadding =>
+                        return Err(Error::Padding),
+                }
             },
         };
         final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
@@ -42,36 +46,36 @@ pub fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
             BufferResult::BufferOverflow => { }
         }
     }
-    Ok(final_result)
+    return Ok(final_result);
 }
 
-// Decrypts a buffer with the given key and iv using
-// AES-256/CBC/Pkcs encryption.
-//
-// This function is very similar to encrypt(), so, please reference
-// comments in that function. In non-example code, if desired, it is possible to
-// share much of the implementation using closures to hide the operation
-// being performed. However, such code would make this example less clear.
-fn decrypt(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
+fn de(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, Error> {
     let mut decryptor = aes::cbc_decryptor(
         aes::KeySize::KeySize256,
         key,
         iv,
         blockmodes::PkcsPadding);
-
     let mut final_result = Vec::<u8>::new();
     let mut read_buffer = buffer::RefReadBuffer::new(encrypted_data);
     let mut buffer = [0; 4096];
     let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
-
     loop {
-        let result = try!(decryptor.decrypt(&mut read_buffer, &mut write_buffer, true));
+        let result = match matchdecryptor.decrypt(&mut read_buffer, &mut write_buffer, true) {
+            Ok(r) => r,
+            Err(e) => {
+                match e {
+                    symmetriccipher::SymmetricCipherError::InvalidLength =>
+                        return Err(Error::Length),
+                    symmetriccipher::SymmetricCipherError::InvalidPadding =>
+                        return Err(Error::Padding),
+                }
+            },
+        };
         final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
         match result {
             BufferResult::BufferUnderflow => break,
             BufferResult::BufferOverflow => { }
         }
     }
-
-    Ok(final_result)
+    return Ok(final_result);
 }
